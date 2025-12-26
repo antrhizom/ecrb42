4037e3cbacdadf4e6c2e902e97c9dde86a62437b
antrhizom <151164845+antrhizom@users.noreply.github.com>
2025-12-26 11:28:46 +0100

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-}
+  apiKey: "AIzaSyD9UWXhEp_RxFEDEzu_ziC42e9EfAcWldk",
+  authDomain: "ecrb42-327b8.firebaseapp.com",
+  projectId: "ecrb42-327b8",
+  storageBucket: "ecrb42-327b8.firebasestorage.app",
+  messagingSenderId: "768507576626",
+  appId: "1:768507576626:web:a22783e2d63f33caa8172b"
+};
 
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
